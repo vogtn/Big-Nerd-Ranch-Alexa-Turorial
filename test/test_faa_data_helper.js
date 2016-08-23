5688c105b078810f108b8a8852a23a1bb6bbbb9a
vogtn <vogtn@uw.edu>
2016-08-23 12:54:19 -0700

    updating data helper

---
+++ test/test_faa_data_helper.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var chai = require('chai');
+
+var expect = chai.expect;
+
+var FAADataHelper = require('../faa_data_helper');
+
+describe('FAADataHelper', function(){
+
+  var subject = new FAADataHelper();
+
+});
