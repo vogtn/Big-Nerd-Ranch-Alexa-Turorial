f7119f3b82b33e794040ebbcdfc5c5e6de57f42d
vogtn <vogtn@uw.edu>
2016-08-23 14:10:57 -0700

    update test for helper

--- test/test_faa_data_helper.js
+++ test/test_faa_data_helper.js
@@ -2,12 +2,16 @@
 
 var chai = require('chai');
 
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
 var expect = chai.expect;
 
 var FAADataHelper = require('../faa_data_helper');
 
+chai.config
+
 describe('FAADataHelper', function(){
-
   var subject = new FAADataHelper();
-
 });
