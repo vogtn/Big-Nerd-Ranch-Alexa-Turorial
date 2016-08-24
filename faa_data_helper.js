9649abc0b922bf8dacb32758f001a41b1da9fd7f
vogtn <vogtn@uw.edu>
2016-08-24 10:47:31 -0700

    updating helper

--- faa_data_helper.js
+++ faa_data_helper.js
@@ -21,4 +21,42 @@ FAADataHelper.prototype.getAirportStatus = function(airportCode){
   };
   return rp(options);
 };
+
+FAADataHelper.prototype.getAirportStatus = function(airportCode) {
+  var options = {
+    method: 'GET',
+    uri: ENDPOINT + airportCode,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+};
+
+FAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
+  var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
+    weather: airportStatus.weather.weather,
+    temp: airportStatus.weather.temp,
+    wind: airportStatus.weather.wind
+  });
+  if (airportStatus.delay === 'true') {
+    var template = _.template('There is currently a delay for ${airport}. ' +
+      'The average delay time is ${delay_time}. ' +
+      'Delay is because of the following: ${delay_reason}. ${weather}');
+    return template({
+      airport: airportStatus.name,
+      delay_time: airportStatus.status.avgDelay,
+      delay_reason: airportStatus.status.reason,
+      weather: weather
+    });
+  } else {
+    //no delay
+    return _.template('There is currently no delay at ${airport}. ${weather}')({
+      airport: airportStatus.name,
+      weather: weather
+    });
+  }
+};
+
+
+
 module.exports = FAADataHelper;
