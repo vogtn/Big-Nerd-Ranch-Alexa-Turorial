72d1f189ab5b933aa0cd0fe971cc90846aa4dcaf
vogtn <vogtn@uw.edu>
2016-08-23 20:57:59 -0700

    updating test

--- test/test_faa_data_helper.js
+++ test/test_faa_data_helper.js
@@ -1,17 +1,22 @@
 'use strict';
 
 var chai = require('chai');
-
 var chaiAsPromised = require('chai-as-promised');
-
 chai.use(chaiAsPromised);
-
 var expect = chai.expect;
-
 var FAADataHelper = require('../faa_data_helper');
 
-chai.config
+chai.config.includeStack = true;
 
 describe('FAADataHelper', function(){
   var subject = new FAADataHelper();
-});
+  var airport_code;
+  describe('#getAirportStatus', function(){
+    context('with a vaild airport code', function(){
+      it('returns matching airport code', function(){
+        airport_code= 'PUNKYBREWSTER';
+        return expect(subject.requestAirportStatus(airport_code)).to.be.rejectedWith(Error);
+      });
+      });
+    });
+  });
